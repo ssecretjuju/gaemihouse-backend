2eac7d13d77f8d7f31329f0f7454048d2a416467
aeju <aejuchoe@naver.com>
2022-10-22 17:04:10 +0900

    # 5 hello world 출력 추가

---
+++ WorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        SpringApplication.run(WorkflowApplication.class, args);
+    }
+
