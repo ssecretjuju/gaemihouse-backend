97d82179cfb9e5a3a554a830e194f1d500fdac8b
gomdoricha <chayoungjun5781@gmail.com>
2022-10-24 10:28:58 +0900

    commit test

--- WorkflowApplication.java
+++
@@ -1,9 +0,0 @@
-@SpringBootApplication
-public class WorkflowApplication {
-
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        SpringBootApplication.run(WorkflowApplication.class, args);
-    }
-    
-}
