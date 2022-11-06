0b9f5c971778db980444d1d6057d6fce6dc641c6
DonghwooCho <whehdgn1001@kookmin.ac.kr>
2022-11-07 07:07:21 +0900

    #62 방 가입 시 정보 저장 기능 구현

--- src/main/java/secretjuju/gaemihouse/shareholder_room/dto/shareholderRoomMemberDTO.java
+++ src/main/java/secretjuju/gaemihouse/shareholder_room/dto/shareholderRoomMemberDTO.java
@@ -1,4 +1,73 @@
 package secretjuju.gaemihouse.shareholder_room.dto;
 
+import javax.persistence.Column;
+
 public class shareholderRoomMemberDTO {
+
+    private int roomMemberCode;
+    private int roomMemberYield;
+    private int joinEvaluateProperty;
+    private int memberCode;
+    private int roomCode;
+    public shareholderRoomMemberDTO() {
+    }
+
+    public shareholderRoomMemberDTO(int roomMemberCode, int roomMemberYield, int joinEvaluateProperty, int memberCode, int roomCode) {
+        this.roomMemberCode = roomMemberCode;
+        this.roomMemberYield = roomMemberYield;
+        this.joinEvaluateProperty = joinEvaluateProperty;
+        this.memberCode = memberCode;
+        this.roomCode = roomCode;
+    }
+
+    public int getRoomMemberCode() {
+        return roomMemberCode;
+    }
+
+    public void setRoomMemberCode(int roomMemberCode) {
+        this.roomMemberCode = roomMemberCode;
+    }
+
+    public int getRoomMemberYield() {
+        return roomMemberYield;
+    }
+
+    public void setRoomMemberYield(int roomMemberYield) {
+        this.roomMemberYield = roomMemberYield;
+    }
+
+    public int getJoinEvaluateProperty() {
+        return joinEvaluateProperty;
+    }
+
+    public void setJoinEvaluateProperty(int joinEvaluateProperty) {
+        this.joinEvaluateProperty = joinEvaluateProperty;
+    }
+
+    public int getMemberCode() {
+        return memberCode;
+    }
+
+    public void setMemberCode(int memberCode) {
+        this.memberCode = memberCode;
+    }
+
+    public int getRoomCode() {
+        return roomCode;
+    }
+
+    public void setRoomCode(int roomCode) {
+        this.roomCode = roomCode;
+    }
+
+    @Override
+    public String toString() {
+        return "shareholderRoomMemberDTO{" +
+                "roomMemberCode=" + roomMemberCode +
+                ", roomMemberYield=" + roomMemberYield +
+                ", joinEvaluateProperty=" + joinEvaluateProperty +
+                ", memberCode=" + memberCode +
+                ", roomCode=" + roomCode +
+                '}';
+    }
 }
---
+++ src/main/java/secretjuju/gaemihouse/shareholder_room/repository/ShareholderRoomMemberRepository.java
@@ -0,0 +1,7 @@
+package secretjuju.gaemihouse.shareholder_room.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import secretjuju.gaemihouse.shareholder_room.model.ShareholderRoomMember;
+
+public interface ShareholderRoomMemberRepository extends JpaRepository<ShareholderRoomMember, Integer> {
+}
