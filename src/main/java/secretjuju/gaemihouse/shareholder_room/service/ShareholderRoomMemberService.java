c78cbaa6968e084582feb5ca5e46e5490833925e
DonghwooCho <whehdgn1001@kookmin.ac.kr>
2022-11-07 08:38:41 +0900

    #64 주주방 회원 현재 수익률 정보 조회 및 저장 기능 구현

--- src/main/java/secretjuju/gaemihouse/shareholder_room/service/ShareholderRoomMemberService.java
+++ src/main/java/secretjuju/gaemihouse/shareholder_room/service/ShareholderRoomMemberService.java
@@ -1,21 +1,33 @@
 package secretjuju.gaemihouse.shareholder_room.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import secretjuju.gaemihouse.shareholder_room.model.ShareholderRoomMember;
+import secretjuju.gaemihouse.shareholder_room.dto.ShareholderRoomMemberDTO;
 import secretjuju.gaemihouse.shareholder_room.repository.ShareholderRoomMemberRepository;
 
 @Service
 public class ShareholderRoomMemberService {
 
     private final ShareholderRoomMemberRepository shareholderRoomMemberRepository;
+    private final ModelMapper modelMapper;
 
-    public ShareholderRoomMemberService(ShareholderRoomMemberRepository shareholderRoomMemberRepository) {
+    public ShareholderRoomMemberService(ShareholderRoomMemberRepository shareholderRoomMemberRepository, ModelMapper modelMapper) {
         this.shareholderRoomMemberRepository = shareholderRoomMemberRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    public ShareholderRoomMemberDTO findShareholderRoomMember(int memberCode) {
+
+        ShareholderRoomMember shareholderRoomMember = shareholderRoomMemberRepository.findByMemberCode(memberCode);
+
+        return modelMapper.map(shareholderRoomMember, ShareholderRoomMemberDTO.class);
     }
 
     @Transactional
-    public void joinShareHolderRoom(ShareholderRoomMember shareholderRoomMember) {
+    public void updateShareholderRoom(ShareholderRoomMember shareholderRoomMember) {
         shareholderRoomMemberRepository.save(shareholderRoomMember);
     }
+
 }
