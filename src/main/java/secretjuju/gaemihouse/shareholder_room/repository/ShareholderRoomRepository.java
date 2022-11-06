de034a31ed1a360318dc8ba3e8ab27f94c1af33c
DonghwooCho <whehdgn1001@kookmin.ac.kr>
2022-11-07 02:22:03 +0900

    #35 주주방 조회 데이터 타입 리스트로 변경

--- src/main/java/secretjuju/gaemihouse/shareholder_room/repository/ShareholderRoomRepository.java
+++ src/main/java/secretjuju/gaemihouse/shareholder_room/repository/ShareholderRoomRepository.java
@@ -3,8 +3,11 @@ package secretjuju.gaemihouse.shareholder_room.repository;
 import org.springframework.data.repository.Repository;
 import secretjuju.gaemihouse.shareholder_room.model.ShareholderRoom;
 
+import java.util.List;
+
 public interface ShareholderRoomRepository extends Repository<ShareholderRoom, Integer> {
 
-    ShareholderRoom findShareholderRoomByMapCode(int mapCode);
+   List<ShareholderRoom> findShareholderRoomByMapCode(int mapCode);
+
 
 }
