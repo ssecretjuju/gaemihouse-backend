92b733ea7a516b2c7d41c3176694efa6a5715ef2
gomdoricha <chayoungjun5781@gmail.com>
2022-11-09 14:49:10 +0900

    #29 개인 정보 삭제 기능 추가

--- src/main/java/secretjuju/gaemihouse/member/service/CustomUserDetailsService.java
+++ src/main/java/secretjuju/gaemihouse/member/service/CustomUserDetailsService.java
@@ -13,6 +13,22 @@ import secretjuju.gaemihouse.member.dto.MemberDTO;
 
 import java.util.Arrays;
 
+/**
+ * <pre>
+ * Class : CustomUserDetailsService
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-10-24       차용준           최초 생성
+ * </pre>
+ *
+ * @author 차용준(최초 작성자)
+ * @version 1(클래스 버전)
+ * @see (참고할 class 또는 외부 url)
+ */
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
