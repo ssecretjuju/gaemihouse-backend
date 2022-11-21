0dd4c500f11588430012c4e14fb5b10f93a1b75d
DonghwooCho <whehdgn1001@kookmin.ac.kr>
2022-11-21 20:23:55 +0900

    conflict 해결

--- src/main/java/secretjuju/gaemihouse/configuration/WebConfig.java
+++
@@ -1,9 +0,0 @@
-package secretjuju.gaemihouse.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-}
